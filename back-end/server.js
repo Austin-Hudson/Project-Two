23e68b0039a7bbf2e6e97c812853b34adf3aaeed
Austin Hudson <arhudson7@gmail.com>
2016-07-21 12:49:40 -0400

    make a backend api call to display place and front end renders it BUT, there is still API key on front end

--- back-end/server.js
+++ back-end/server.js
@@ -2,10 +2,44 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
+var request = require('request');
 var app = express();
+var PORT = 3000;
 
 /* let's add the ability ajax to our server from anywhere! */
 app.use(cors());
 
 /* extended:true = put it in an obj */
 app.use(bodyParser.urlencoded({extended: true}));
+
+/* marvel search */
+app.post('/restaurant/search', function(req, res) {
+
+
+  var baseUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+  var apiKeyQueryString = "?key=";
+  var GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+  var restaurant = "&query=" + req.body.queryString;
+  var lat = req.body.lat;
+  var long = req.body.long;
+  var location = "&location=" + lat + ',' + long;
+  var radius = "&radius=" + req.body.radius;
+  var fullQuery = baseUrl + apiKeyQueryString + GOOGLE_MAPS_API_KEY + restaurant + location + radius;
+  console.log("fullQuery:", fullQuery); // prints to terminal
+
+  request({
+    url: fullQuery,
+    method: 'GET',
+    callback: function(error, response, body) {
+      // console.log(body);
+      // console.log(response);
+      res.send(body);
+    }
+  })
+
+}); // end post request
+
+/* tell our app where to listen */
+app.listen(3000, function(){
+  console.log('listen to events on port:', PORT);
+});
--- front-end/main.js
+++ front-end/main.js
@@ -1,3 +1,87 @@
 document.addEventListener("DOMContentLoaded", function(){
   console.log("main.js is loaded.");
+
+  var searchBtn = document.querySelector("#search-location");
+  var latitude, longitude;
+  var currentLocation;
+  var map;
+
+  // URL to BE:
+  var url = 'http://localhost:3000';
+
+  searchBtn.addEventListener("click", function(){
+    navigator.geolocation.getCurrentPosition(success,error)
+    userInput = document.querySelector("#location-text").value;
+  });
+
+  function displayMap(response){
+    currentLocation =  new google.maps.LatLng(latitude,longitude);
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: currentLocation,
+    zoom: 15
+  });
+    displayResults(response.results);
+  }
+
+  function displayResults(results) {
+    console.log(results);
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+  function createMarker(res) {
+      var marker = new google.maps.Marker({
+        position: res.geometry.location,
+        map: map
+      })
+      console.log(marker);
+      // infoWindow = new google.maps.InfoWindow({
+      //     content: ""
+      // });
+      // google.maps.event.addListener(marker, "click", function(){
+      //   infoWindow.setContent(res.name + "<button type='button' id='fav'>Fav it!</button");
+      //   infoWindow.open(map, this);
+      //
+      //   document.querySelector("#fav").addEventListener("click", function(){
+      //     var location = {
+      //       name: res.name,
+      //       address: res.formatted_address
+      //     }
+      //     console.log(location);
+      //     $.post('http://localhost:3000/locations', location, function(response){
+      //       console.log("Response:", response);
+      //     })
+      //   })
+
+      // });
+
+    }
+  function success(pos) {
+    latitude = pos.coords.latitude;
+    longitude = pos.coords.longitude;
+
+    //send data BE
+    var data = {
+      queryString: userInput,
+      lat: latitude,
+      long: longitude,
+      radius: '30'
+    };
+
+    $.ajax({
+     url: url + '/restaurant/search',
+     method: 'POST',
+     data: data,
+     dataType: 'json'
+   }).done(function(response) {
+      displayMap(response);
+   }); // end ajax
+  }
+
+  function error(){
+    var results = document.querySelector(".results");
+    var p = document.createElement("p");
+    p.innerHTML = "An error has occured.";
+    results.appendChild(p);
+  }
 });
